90c9e18086b9f071aabef27d96c0474277884152
Marwan Aziz <marwan@marwanaziz.net>
2024-05-28 22:01:29 +0100

    update upload script

--- updateRepository.sh
+++ updateRepository.sh
@@ -1,52 +1,55 @@
+#!/bin/bash
+
 NOCOLOR='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
 REPOSITORY="git@github.com:MarwanAziz/kotlinMultiplatform.git"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-REPO_VERSION=$(cat $DIR/../build.gradle.kts | grep -m 1 frameworkVersion | cut -d'"' -f 2)
+REPO_VERSION=$(grep -m 1 frameworkVersion "$DIR/../build.gradle.kts" | cut -d'"' -f 2)
 TMPDIR=$(mktemp -d)
 TMPFILE=$(mktemp)
 
 trap _cleanup EXIT
 
 function _cleanup() {
-    rm -rf $TMPDIR
-    rm $TMPFILE
+    rm -rf "$TMPDIR"
+    rm "$TMPFILE"
 }
 
 function _checkTagVersion() {
-    cd $TMPDIR
+    cd "$TMPDIR" || exit
 
-    git tag >$TMPFILE
+    git tag >"$TMPFILE"
 
-    if grep -q $REPO_VERSION $TMPFILE; then
-        echo "${RED}Tag $REPO_VERSION already exists${NOCOLOR}"
-        echo "${RED}Update the tag in 'buildSrc/src/main/java/Config.kt' and run 'create_cocoapod.sh' again.${NOCOLOR}"
+    if grep -q "$REPO_VERSION" "$TMPFILE"; then
+        echo -e "${RED}Tag $REPO_VERSION already exists${NOCOLOR}"
+        echo -e "${RED}Update the tag in 'buildSrc/src/main/java/Config.kt' and run 'create_cocoapod.sh' again.${NOCOLOR}"
         exit 1
     fi
 }
 
 function main() {
     echo "-- Cloning repository --"
-    git clone $REPOSITORY $TMPDIR
+    git clone "$REPOSITORY" "$TMPDIR" || { echo -e "${RED}Failed to clone repository${NOCOLOR}"; exit 1; }
+    echo "-- Repository cloned successfully --"
 
     _checkTagVersion
 
     echo "-- Copy framework and podspec --"
-    cd $DIR/..
-    cp -R /build/bin/universal/release/*.framework $TMPDIR
-    cp *.podspec $TMPDIR
+    cd "$DIR/.." || exit
+    cp -R build/bin/universal/release/*.framework "$TMPDIR" || { echo -e "${RED}Failed to copy framework${NOCOLOR}"; exit 1; }
+    cp *.podspec "$TMPDIR" || { echo -e "${RED}Failed to copy podspec${NOCOLOR}"; exit 1; }
 
-    cd $TMPDIR
+    cd "$TMPDIR" || exit
 
     echo "-- Add files, make commit and tag --"
     git add --all
-    git commit -m "Release $REPO_VERSION"
-    git tag -a $REPO_VERSION -m "Release $($REPO_VERSION)"
+    git commit -m "Release $REPO_VERSION" || { echo -e "${RED}Failed to commit changes${NOCOLOR}"; exit 1; }
+    git tag -a "$REPO_VERSION" -m "Release $REPO_VERSION" || { echo -e "${RED}Failed to create tag${NOCOLOR}"; exit 1; }
 
     echo "-- Push to repository --"
-    git push --atomic origin master $REPO_VERSION
+    git push --atomic origin master "$REPO_VERSION" || { echo -e "${RED}Failed to push to repository${NOCOLOR}"; exit 1; }
 }
 
 main
